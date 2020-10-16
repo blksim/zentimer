ef5973a8432ab3bd0616b31da1680e07556cdbce
blksim <blakesim3@gmail.com>
2020-10-16 15:52:55 +0900

    Add Layout with full size background image

--- zentimer-frontend/src/App.js
+++ zentimer-frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import './App.css';
+import Layout from './container/Layout/Layout';
 
 function App() {
   return (
     <div className="App">
-     hello there!
+     <Layout />
     </div>
   );
 }
---
+++ zentimer-frontend/src/container/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import classes from './Layout.module.css';
+
+class Layout extends Component {
+  render () {
+    return <div className={classes.Layout}></div>
+  }
+};
+
+export default Layout;
