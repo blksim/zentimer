8b1fc7e6f21f38bf9c0ebc575c78244433f25cf6
blksim <blakesim3@gmail.com>
2020-10-31 21:51:11 +0900

    Setup backend

--- zentimer-backend/index.js
+++ zentimer-backend/index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+  res.header("Access-Control-Allow-Headers", "Origin, X-requested-With, Content-Type, Accept");
+  next();
+})
+
 app.get('/', (req, res) => {
   res.send('Hello world!')
+});
+
+app.post('/signup', (req, res) => {
+  console.log('it works');
+  res.send(req.body);
 });
 
 app.listen(port, () => {
