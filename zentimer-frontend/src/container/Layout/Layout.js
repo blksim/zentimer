3a9e26f40a02eafd79ab4d3ec1b1b0631478de16
blksim <blakesim3@gmail.com>
2020-11-13 23:13:35 +0900

    Organize code

--- zentimer-frontend/src/container/Layout/Layout.js
+++ zentimer-frontend/src/container/Layout/Layout.js
@@ -2,208 +2,37 @@ import React, { Component } from 'react';
 
 import classes from './Layout.module.css';
 import Header from '../../components/Header/Header';
-import Main from '../../components/Main/Main';
-import Timer from '../../components/Timer/Timer';
-import Tasks from '../../components/Tasks/Tasks';
+import Modal from '../../components/UI/Modal/Modal';
+import Main from '../../container/Main/Main';
 import Footer from '../../components/Footer/Footer';
-import axios from '../../shared/axios';
 
+import { Router } from '@reach/router';
+ 
 class Layout extends Component {
   state = {
-    quotes: [
-      'Self-control is strength. Calmness is mastery.'
-    ],
-    focusDuration: 25,
-    breakDuration: 5,
-    timeLeft: '',
-    streakLeft: 0,
-    streakFinished: 0,
-    streakTitle: 'Meditation',
-    isFocusMode: false,
-    isBreakMode: false,
-    isFinished: false,
-    title: '',
-    count: 0,
-    tasks: []
-  };
-  
-  getZeroPrefixedTime = (time) => {
-    if (time.min < 10) {
-      time.min = '0' + time.min;
-    }
-    if (time.sec < 10) {
-      time.sec = '0' + time.sec;
-    }
-    return time.min + ' : ' + time.sec;
+    nav: '',
+    isAuthenticated: false,
+    isOauth: ''
   };
 
-  countdown = () => {
-    let sec = 60;
-    let min = this.state.focusDuration;
-    const intervalId = setInterval(() => {
-      if (min === 0) {
-        clearInterval(intervalId);
-        this.setState({ isFinished: true });
-      }
-      if (sec === 0) {
-        sec = 60;
-        min--;
-      }
-      sec--;
-      this.setState({
-        timeLeft: this.getZeroPrefixedTime({
-          min: min,
-          sec: sec
-        })
-      });
-    }, 1000);
-  };
-
-  titleChangeHandler = (event, id) => {
-    const input = event.target.value;
-    const tasks = [...this.state.tasks];
-    if (input && id) {
-      tasks.map((task) => {
-        if (task.id === id) task.id = input;
-        return tasks;
-      });
-     this.setState(tasks);
-    }
-    if (input) {
-      this.setState({ title: input });
-    }
-  };
-
-  saveClickHandler = () => {
-    axios.post('/tasks.json', {
-      title: this.state.title,
-      count: this.state.count
-    })
-    .then((res) => {
-      this.setState({
-        tasks: [...this.state.tasks, {
-          id: res.data.name,
-          title: this.state.title,
-          count: this.state.count
-        }]
-      });
-    })
-    .catch((err) => {
-      console.log('err');
-    })
-  };
-
-  minusClickHandler = (id) => {
-    if (!id) {
-      if (this.state.count <= 0) {
-        this.setState({count: 0}) 
-      } else {
-        this.setState({count: this.state.count-1});
-      }
-    } else {
-      const tasks = [...this.state.tasks];
-      tasks.map((task) => {
-        if (task.id === id) {
-          task.count = task.count - 1;
-          if (task.count <= 0) {
-            task.count = 0
-          } 
-        }
-        return tasks;
-      });
-     this.setState(tasks);
-    }
-  };
-
-  plusClickHandler = (id) => {
-    const maximum = (24 * 60) / (this.state.focusDuration + this.state.breakDuration);
-    if (!id) {
-      if (this.state.count >= maximum) {
-        this.setState({count: maximum})
-      } else {
-        this.setState({count: this.state.count + 1});
-      }
-    } else {
-      const tasks = [...this.state.tasks];
-      tasks.map((task) => {
-        if (task.id === id) {
-          task.count = task.count + 1;
-          if (task.count >= maximum) {
-            task.count = maximum
-          } 
-        }
-        return tasks;
-      });
-      this.setState(tasks);
-    }
-  }
-
-    // const updateClickHandler = (id) => {
-  //   const task = tasks.filter((task) => task.id === id);
-  //   axios.put('/tasks/' + id +'.json', task[0])
-  //   .then((res) => {
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
-  // };
-
-  // const deleteClickHandler = (id) => {
-  //   axios.delete('/tasks/' + id +'.json')
-  //   .then((res) => {
-  //     const tasks = [ ...props.tasks ];
-  //     const updated = tasks.filter(task => { return task.id !== id });
-  //     setTasks(updated);
-  //   })
-  //   .catch((err) => {
-  //     console.log('err');
-  //   });
-  // };
-
-  componentDidMount() {
-    //this.countdown();
-    axios.get('https://zentimer-2fbe5.firebaseio.com/tasks.json')
-      .then((res) => {
-        const tasks = [];
-        for (const key in res.data) {
-          res.data[key].id = key;
-          tasks.push(res.data[key]);
-        }
-        this.setState({ tasks: tasks })
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  navHandler = (event) => {
+    this.setState({ nav: event.target.text })
   };
 
   render () {
-    const style = {
-      display: 'flex'
-    }
     return (
     <div className={classes.Layout}>
-      <Header />
-      {/* <Main text={this.state.quotes[0]}/> */}
-      <main style={style}>
-      <Timer 
-        timeLeft={this.state.timeLeft}
-        title={this.state.title}
-        streakLeft={this.state.streakLeft}
-        streakFinished={this.state.streakFinished}/>
-      <Tasks 
-        focusDuration={this.state.focusDuration}
-        breakDuration={this.state.breakDuration}
-        tasks={this.state.tasks}
-        titleChange={(event) => this.titleChangeHandler(event)}
-        saveClick={() => this.saveClickHandler()}
-        plusClick={(event) => this.plusClickHandler(event)}
-        minusClick={(event) => this.minusClickHandler(event)}
-        updateClick={() => this.updateClickHandler(id)}
-        deleteClick={() => this.deleteClickHandler(id)}
+      <Header
+        auth={this.state.isAuthenticated}
+        click={(event) => this.navHandler(event)}
       />
-      </main>
+      <Router>
+        <Modal path={"/" + this.state.nav} />
+        <Main path={this.state.isAuthenticated ? '/tasks' : '/'}/>
+      </Router> 
       <Footer />
-    </div>)
+    </div>
+    )
   }
 };
 
